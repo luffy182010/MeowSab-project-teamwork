1f52e9599c5789a4c8bbc31d85a434952f24b5ff
RIOTIX <luffysinbai@gmail.com>
2026-06-01 21:58:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
 // script.js
 
-console.log("start");
+window.onload = function () {
+
+    var form = document.querySelector(".login-box");
+
+    form.onsubmit = function (event) {
+
+        event.preventDefault();
+
+        var usernameInput = document.querySelector("input[type='text']");
+        var passwordInput = document.querySelector("input[type='password']");
+
+        var username = usernameInput.value;
+        var password = passwordInput.value;
+
+        if (username === "" || password === "") {
+            alert("Please fill in all fields.");
+            return;
+        }
+
+        if (username === "admin" && password === "1234") {
+            alert("Login successful");
+        } else {
+            alert("Incorrect username or password");
+        }
+    };
+
+};
